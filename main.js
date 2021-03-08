919a674658a78d43237838ba2654f99c10de6b3d
wvwall <waltervelardo@gmail.com>
2021-03-08 09:52:30 +0100

    Completo

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
-// Stampare numeri da 1 a 100 && multipli3 = Fizz, Multilipli5 = Buzz, Multipli3 5 = FizzBuzz
+// Stampare numeri da 1 a 100 ;
+// Multipli3 = Fizz ;
+// Multilipli5 = Buzz ;
+// Multipli3 5 = FizzBuzz ;
+
 
 for (var i = 1; i <= 100; i++) {
+
     if ((i % 3 == 0) && (i % 5 == 0)) {
         console.log("Fizz-Buzz");
         document.getElementById("lista").innerHTML += "<li>" + "Fizz-Buzz" + "</li>";
@@ -10,7 +15,7 @@ for (var i = 1; i <= 100; i++) {
     } else if (i % 5 == 0){
         document.getElementById("lista").innerHTML += "<li>" + "Buzz" + "</li>";
         console.log("Buzz");
-    }else {
+    } else {
         document.getElementById("lista").innerHTML += "<li>" + i + "</li>";
         console.log(i);
     }
